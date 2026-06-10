c0f39ef8bd5e6057199abc6b59f77d097e9fcd91
ARGI-BERRI <me@endroit.net>
2026-06-10 17:35:44 +0900

    feat: wip

--- src/background.js
+++ src/background.js
@@ -1,7 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "save-to-immich",
-    title: "Save to Immich",
+    id: "iic-save",
+    title: "Upload to Immich",
     contexts: ["image"],
   });
+});
+
+chrome.contextMenus.onClicked.addListener(async (clickData, _) => {
+  if (clickData.menuItemId === "iic-save") {
+    const imageUrl = clickData.srcUrl;
+    const settings = await chrome.storage.sync.get(["serverUrl", "apiKey"]);
+
+    if (!settings.serverUrl || !settings.apiKey) {
+      console.error("Server URL or API key not set in settings.");
+      return;
+    }
+
+    console.log("Saving image to Immich:", {
+      serverUrl: settings.serverUrl,
+      imageUrl,
+    });
+  }
 });
